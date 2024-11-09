18fc8b440e96373363f51305279e18892ffb5630
Hristo Karchev <x_karchev@mail.bg>
2024-11-09 18:01:34 +0200

    fix/ added links to pages

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -22,10 +22,10 @@ function Navigation() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#pabloPage">Pablo Picasso</Nav.Link>
-                            <Nav.Link href="#suzannePage">Suzanne Valadon</Nav.Link>
-                            <Nav.Link href="#joanPage">Joan Miro</Nav.Link>
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/pabloPage">Pablo Picasso</Nav.Link>
+                            <Nav.Link href="/suzannePage">Suzanne Valadon</Nav.Link>
+                            <Nav.Link href="/joanPage">Joan Miro</Nav.Link>
                             <Nav.Link href="#contactUs">Contact Us</Nav.Link>   
                         </Nav>
                     </Navbar.Collapse>
