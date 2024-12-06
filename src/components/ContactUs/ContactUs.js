5d4cdeda7d5dc4be329057624a9f321aa2426df4
Hristo Karchev <x_karchev@mail.bg>
2024-12-06 18:40:00 +0200

    fix/ map added

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -36,6 +36,27 @@ function ContactUs() {
                     </div>
                 </Form>
             </Container>
+            <div className='google-map'>
+                <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158979.46403020338!2d27.789416963749783!3d43.21193883425022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a4538baaf3d7a1%3A0x5727941c71a58b7c!2z0JLQsNGA0L3QsA!5e0!3m2!1sbg!2sbg!4v1733502412347!5m2!1sbg!2sbg" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            {/* <Container fluid>
+                <div className='contact-info'>
+                    <ul>
+                        <li>
+                            <i className="fas fa-envelope"></i>
+                            hello@domain.com
+                        </li>
+                        <li>
+                            <i className="fas fa-phone"></i>
+                            000-000-0000
+                        </li>
+                        <li>
+                            <i className="fas fa-map-marker-alt"></i>
+                            London, United Kingdom
+                        </li>
+                    </ul>
+                </div>
+            </Container> */}
         </section>
     )
 }
