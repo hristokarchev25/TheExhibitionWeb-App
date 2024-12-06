d3b9a35a6db71c1c284c7a4ba2124f7a26f8f6cf
Hristo Karchev <x_karchev@mail.bg>
2024-12-06 16:43:37 +0200

    fix/ added hero data and hero component

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,45 +1,66 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
+import "./ImageSlider.css";
+
+var heroData = [
+    {
+        id: 1,
+        image: require('../../assets/img-hero1.jpg'),
+        title: 'Pablo Picasso',
+        description: 'Every child is an artist. The problem is how to remain an artist once he grows up.',
+        link: 'https://www.google.com',
+        btnText: 'Pablo Page'
+    },
+    {
+        id: 2,
+        image: require('../../assets/img-hero2.jpg'),
+        title: 'Suzanne Valadon',
+        description: 'I had great masters. I took the best of them of their teachings, of their examples. I found myself, I made myself, and I said what I had to say.',
+        link: 'https://www.facebook.com',
+        btnText: 'Pablo Page'
+    },
+    {
+        id: 3,
+        image: require('../../assets/img-hero3.jpg'),
+        title: 'Joan Miró',
+        description: 'It is the young people who interest me, and not the old dodos. If I go on working, it is for the year 2000, and for the people of tomorrow.',
+        link: 'https://www.twitter.com',
+        btnText: 'Pablo Page'
+    },
+    {
+        id: 4,
+        image: require('../../assets/img-hero4.jpg'),
+        title: 'Art Shop',
+        description: 'It is the young people who interest me, and not the old dodos. If I go on working, it is for the year 2000, and for the people of tomorrow.',
+        link: 'https://www.twitter.com',
+        btnText: 'Art Shop'
+    },
+]
 
 function ImageSlider() {
     return (
-        <Carousel data-bs-theme="dark">
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=f5f5f5"
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=eee"
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h5>Third slide label</h5>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+        <section className='hero-block'>
+            <Carousel>
+                {
+                    heroData.map(hero => {
+                        return (
+                            <Carousel.Item key={hero.id}>
+                                <img
+                                    className="d-block w-100"
+                                    src={hero.image}
+                                    alt={"slide " + hero.id}
+                                />
+                                <Carousel.Caption>
+                                    <h2>{hero.title}</h2>
+                                    <p>{hero.description}</p>
+                                    <a className="btn btn-primary" href={hero.link}>{hero.btnText}</a>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        );
+                    })
+                }
+            </Carousel>
+        </section>
     )
 }
 
