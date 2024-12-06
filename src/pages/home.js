9424bc25dac2ef6924ad3affeb8833bb801c5b59
Hristo Karchev <x_karchev@mail.bg>
2024-12-06 17:35:02 +0200

    fix/about page created + styles and image

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from '../components/Navigation/Navigation';
 import ImageSlider from '../components/ImageSlider/ImageSlider';
+import About from '../components/About/About';
 
 function home() {
     return (
         <>
             <Navigation />
             <ImageSlider />
+            <About />
         </>
     )
 }
