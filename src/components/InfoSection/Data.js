a7daad92cf9a0b4f9ff6ef63a48a9d024febbdc4
Hristo Karchev <x_karchev@mail.bg>
2024-12-10 13:09:16 +0200

    add/ data objs created for the pages

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,27 @@
+import img1 from '../../assets/img1.jpg';
+
+
+
+export const pabloObj = {
+    id: 'pablo',
+    nameOfPage: 'Pablo Picasso',
+    subtitle: 'learn more about Pablo',
+    image: img1,
+    descriptionParahraph: 'PP was born in ...At The Exhibition Web App, we make it our mission to help you discover buy from the best emerging artists around the world. Whether you’re looking to discover a new artist, add a statement piece to your home, or commemorate an important life event, The Exhibition Web App is your portal to thousands of original works by todays top artists.”'
+};
+
+export const suzanneObj = {
+    id: 'suzanne',
+    nameOfPage: 'Suzanne Valadon',
+    subtitle: 'learn more about Suzanne',
+    image: img1,
+    descriptionParahraph: 'SV was born in ...'
+};
+
+export const joanObj = {
+    id: 'joan',
+    nameOfPage: 'Joan Miro',
+    subtitle: 'learn more about Joan',
+    image: img1,
+    descriptionParahraph: 'JM was born in ...'
+};
