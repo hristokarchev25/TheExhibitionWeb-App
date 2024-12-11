284f7879f4c574545d3f0f73c9ce48caf129bd5d
Hristo Karchev <x_karchev@mail.bg>
2024-12-11 17:03:23 +0200

    add/firebase config added

---
+++ src/utils/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBpzjRssd7UfQWFw4tz8P1pXIbE6kiq0YQ",
+    authDomain: "the-exhibition-app.firebaseapp.com",
+    projectId: "the-exhibition-app",
+    storageBucket: "the-exhibition-app.firebasestorage.app",
+    messagingSenderId: "551791094162",
+    appId: "1:551791094162:web:c7c59bcffd4d62e7e7394b"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
+
+export const auth = firebase.auth();
