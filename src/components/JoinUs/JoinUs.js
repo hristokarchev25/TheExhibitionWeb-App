5630b24801015b97d7e759fed6ad675478c32878
Hristo Karchev <x_karchev@mail.bg>
2024-12-11 13:37:41 +0200

    fix/ styles on join us form

--- src/components/JoinUs/JoinUs.js
+++ src/components/JoinUs/JoinUs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./JoinUs.module.css";
+import "./JoinUs.css";
 import joinImg from '../../assets/joinUs.png';
 import {
     MDBBtn,
@@ -16,7 +16,7 @@ import {
 
 function JoinUs() {
     return (
-        <MDBContainer className="my-5">
+        <MDBContainer className="my-5 main-container">
 
             <MDBCard>
                 <MDBRow className='g-0'>
@@ -26,7 +26,7 @@ function JoinUs() {
                     </MDBCol>
 
                     <MDBCol md='6'>
-                        <MDBCardBody className='d-flex flex-column'>
+                        <MDBCardBody className='d-flex flex-column main-card'>
 
                             <div className='d-flex flex-row mt-2'>
                                 <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }} />
@@ -35,14 +35,14 @@ function JoinUs() {
 
                             <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: '1px' }}>Sign into your account</h5>
 
-                            <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" />
-                            <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" />
+                            <MDBInput className='form-label' wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" />
+                            <MDBInput className='form-label' wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" />
 
-                            <MDBBtn className="mb-4 px-5" color='dark' size='lg'>Login</MDBBtn>
+                            <MDBBtn className="mb-4 px-5" color='primary' size='lg'>Login</MDBBtn>
                             <a className="small text-muted" href="#!">Forgot password?</a>
-                            <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>Don't have an account? <a href="#!" style={{ color: '#393f81' }}>Register here</a></p>
+                            <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>Don't have an account? <a href="/registration" style={{ color: '#393f81' }}>Register here</a></p>
 
-                            <div className='d-flex flex-row justify-content-start'>
+                            <div className='d-flex flex-row justify-content-center'>
                                 <a href="#!" className="small text-muted me-1">Terms of use.</a>
                                 <a href="#!" className="small text-muted">Privacy policy</a>
                             </div>
