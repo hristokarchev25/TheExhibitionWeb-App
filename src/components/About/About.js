53bdbecbd273b72821c75ee65b999e6df364a09d
Hristo Karchev <x_karchev@mail.bg>
2024-12-15 01:02:28 +0200

    fix/ about us lorem text fixed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -20,9 +20,9 @@ function About() {
                         <Image src={img1} />
                     </Col>
                     <Col sm={6}>
-                        <p>“At The Exhibition Web App, we make it our mission to help you discover and buy from the best emerging artists around the world. Whether you’re looking to discover a new artist, add a statement piece to your home, or commemorate an important life event, The Exhibition Web App is your portal to thousands of original works by today’s top artists.”</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae alias ex error distinctio laudantium recusandae, iure. Vel nisi soluta, minus consectetur reiciendis laborum, laudantium perspiciatis quos molestias quam eum.</p>
-                        <p>Lorem it. Reprehenderit dicta veritatis sint quidem asperiores magni eaque praesentium temporibus, magnam, eveniet consequatur possimus nihil? Nemo deserunt repudiandae soluta neque doloribus, quo!</p>
+                        <p>At The Exhibition Web App, we make it our mission to help you discover and buy from the best emerging artists around the world. Whether you’re looking to discover a new artist, add a statement piece to your home, or commemorate an important life event, The Exhibition Web App is your portal to thousands of original works by today’s top artists.</p>
+                        <p>At The Exhibition Web App you have the chance to learn about some of the most popular artists of the past. Greats like Pablo Picasso, Suzanne Valadon and Joan Miró. You have the opportunity to read in detail about some of their most famous works.</p>
+                        <p>We are proud to present a collection of hobby paintings and photos by young artists! Enjoy!</p>
                         <p><b>Hristo Karchev</b><br />Creator</p>
                     </Col>
                 </Row>
