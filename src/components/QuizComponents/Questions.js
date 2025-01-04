976b771534ca4b147f936a91ddf3da0b6139199a
Hristo Karchev <x_karchev@mail.bg>
2025-01-04 14:09:46 +0200

    add/ quiz component created and added styles

---
+++ src/components/QuizComponents/Questions.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+//import { Link } from 'react-router-dom';
+import { db } from '../../utils/firebase';
+import "./Questions.css";
+
+function Questions() {
+    const [questions, setQuestion] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
+
+
+    useEffect(() => {
+        const getPostsFromFirebase = [];
+        const subscriber = db
+            .collection("questions")
+            .onSnapshot((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    getPostsFromFirebase.push({
+                        ...doc.data(), //spread operator
+                        key: doc.id, // `id` given to us by Firebase
+                    });
+                });
+                setQuestion(getPostsFromFirebase);
+                setLoading(false);
+            });
+
+        // return cleanup function
+        return () => subscriber();
+    }, [loading]);
+
+    if (loading) {
+        return <h1>loading firebase data...</h1>;
+    }
+
+    const handleAnswerOptionClick = (isCorrect) => {
+        if (isCorrect) {
+            setScore(score + 1);
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
+
+    return (
+        <div className='question__page'>
+            <motion.div>
+                <div className='questions'>
+                    {showScore ? (
+                        <div className='score__section'>
+                            You scored {score} out of {questions.length}
+                            <a href="/artQuiz"><button className="button-style">Try again?</button></a>
+                        </div>
+                    ) : (
+                        <>
+                            <div className='question__section'>
+                                <div className='question__text'>{questions[currentQuestion].questionText}</div>
+                            </div>
+                            <div className='answer__section'>
+
+                                {questions[currentQuestion].answerOptions.map((answerOption) => (
+
+                                    <button className="question__btn" onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}><span className="answer__prefix">{answerOption.answerPrefix}</span>{answerOption.answerText}</button>
+
+                                ))}
+
+                            </div>
+                        </>
+                    )}
+                </div>
+            </motion.div>
+        </div>
+
+    )
+}
+
+export default Questions
